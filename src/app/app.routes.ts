62142d2c0e09199baabce17d61265d63a07b1568
Carlos Haro Polo <carlosisaachp18@gmail.com>
2025-03-07 18:21:28 -0500

    7-03.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component') ,/* tienes que agregar la palbara defaul al componente */
+    children:[
+      {
+        path: 'trending',
+        loadComponent:()=>import('./gifts/pages/trending-page/trending-page.component')
+      },
+      {
+        path: 'search',
+        loadComponent:()=>import('./gifts/pages/search-page/search-page.component')
+      },
+      {
+        path:'**',
+        redirectTo:'trending'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
---
+++ src/app/gifts/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gits-side-menu-header',
+  imports: [],
+  templateUrl: './side-menu-header.component.html',
+})
+export class SideMenuHeaderComponent { }
